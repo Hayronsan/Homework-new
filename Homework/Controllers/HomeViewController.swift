1b0ea76fe5bfcd161e6faddabb022cb9b4907966
Hayronsan <hayronsan@gmail.com>
2024-04-12 19:53:29 +0500

    Initial Commit

---
+++ Homework/Controllers/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeViewController.swift
+//  Homework
+//
+//  Created by Nodir on 12/04/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initViews()
+
+    }
+
+//   Mark: - Method
+    
+    func initViews(){
+        
+    }
+    
+    func setNavigationBar(){
+        
+    }
+    
+//   Mark: - Action
+    
+    
+    
+    
+    
+//   Mark: - Table View
+    
+    
+    
+
+}
